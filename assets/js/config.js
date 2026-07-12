80b772378c2f7380f7d7959a967238d751a730b3
adssuccessph <ads.success.ph21@gmail.com>
2026-07-12 10:11:12 +0800

    Add files via upload

---
+++ assets/js/config.js
@@ -0,0 +1,10 @@
+/*
+  BARIKAN LANDING PAGE CONFIG
+  1) Deploy apps-script/Code.gs as a Google Apps Script Web App.
+  2) Paste the /exec URL below.
+  3) Change demoMode to false.
+*/
+window.BARIKAN_CONFIG = {
+  googleScriptUrl: "https://script.google.com/macros/s/AKfycbz1Yv91w5jRosD_K3MdZXSjErPlS9211eFHNyGy4WQaf8EYrGU1uAu8FpGB12t8CSADww/exec",
+  demoMode: false
+};
