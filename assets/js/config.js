09d52f536fd11eebdfebeb5e3515427d7f54ac47
adssuccessph <ads.success.ph21@gmail.com>
2026-07-15 19:37:56 +0800

    Delete assets directory

--- assets/js/config.js
+++
@@ -1,5 +0,0 @@
-/* BARikan Event Booking - LIVE CONFIG */
-window.BARIKAN_CONFIG = {
-  googleScriptUrl: "https://script.google.com/macros/s/AKfycbw4_c3tu5QRjcYO98iRltyMPteT1hX7PNbEjNw7dui_2Oka4-ZoF6FCP847KbsSZbyB-Q/exec",
-  demoMode: false
-};
